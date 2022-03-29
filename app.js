6de14a7d248e1a075122a2056f4feb0cc201687d
Zaheer <zaheerbabar955@gmail.com>
2022-03-29 12:13:02 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,271 @@
+                               // Chapter no 1
+
+// q:1
+// alert("hello");
+
+// q:2
+// document.write("hello world")
+
+// q:3
+// alert("welcome to js land... \nhappy coding!")
+
+// q:4
+// alert("welcome to js land...")
+// alert("happy coding!")
+
+// q:5
+// console.log("i can run js in my browser's console")
+
+                                //Chapter no 2
+
+//q:1
+//var userName = "Zaheer";
+
+//q:2
+//var myName = "Zaheer babar"
+
+//q:3
+// var massage = "Hello World";
+// alert(massage);
+
+// q:4
+// var myName ="zaheer";
+// var age = 21;
+// var skills = "web and mobile app development";
+// alert(myName);
+// alert(age);
+// alert(skills); 
+
+//q:5
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+//q:6
+// var email = "zaheerbabar955@gmail.com";
+// alert("My email is" + " " + email);
+
+//q:7
+// var book = "A smarter way to learn JavaScript";
+// alert("i am trying to learn from a book" + " " + book)\
+
+//q:8
+// document.write("Woohoo! i can write HTML content through Js")
+
+//q:9
+// var a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(a);
+
+                                 // Chapter no 3
+
+//q:1
+// var age = 21;
+// alert("My age is"+ " " +age);        
+
+//q:2
+// alert("You have visited 1 time");
+// alert("You have visited 2 time");
+// alert("You have visited 3 time");
+// alert("You have visited 4 time");
+// alert("You have visited 5 time");
+// alert("You have visited 6 time");
+// alert("You have visited 7 time");
+// alert("You have visited 8 time");
+// alert("You have visited 9 time");
+// alert("You have visited 10 time");
+
+//q:3
+// var birthYear = 2000; 
+// document.write(birthYear);
+
+//q:4
+// var userName = prompt("Your name");
+// var brand = prompt("which brand you want");
+// var quantity = +prompt("Quantity");
+// alert(userName + " " + "confirmed his order of" + " " + quantity + " " + "pieces of" + " " + brand);
+
+                            //Chapter no 4
+        
+//q:1
+// var myName = "zaheer"
+// var myTeacher = "jaffar aman"
+// var myInstitude = "BMJ"
+
+//q:2
+//    5 legal variable names
+// var name = 1;
+// var name1 = 2;
+// var $name = 3;
+// var camelCase = 4;
+// var snake_case = 5;
+//   5 illegal variable names 
+// var &name = 1;
+// var 1name = 2;
+// var my name = 3;
+// var name-case = 4;
+// var %case = 5;
+
+//q:3
+// a document.write( "<h1>Rules for naming JS variables</h1>")
+// b document.write(" Variable names can only contain numbers and $ For example $my_1stVariable </br>")
+// c document.write(" Variables must begin with a letter, $ or _. For example $name, _name or name </br>")
+// d document.write("Variable names are case sensitive </br>")
+// e document.write(" Variable names should not be JS kehwords")
+
+                            //Chapter no 5
+
+// //q:1
+// var num1 = 5
+// var num2 = 7
+// document.write(num1 + num2) 
+
+// //q:2.a
+// var num1 = 5
+// var num2 = 7
+// document.write(num1 - num2) 
+
+// //q:2.b
+// var num1 = 5
+// var num2 = 7
+// document.write(num1 * num2) 
+
+// //q:2.c
+// var num1 = 5
+// var num2 = 7
+// document.write(num1 / num2) 
+
+// //q:2.d
+// var num1 = 5
+// var num2 = 7
+// document.write(num1 % num2)         
+
+//q:3
+// var value = "value is undefined";
+// document.write(value);
+// value = 5;
+// document.write(value + "</br>");
+// var value = ++value;
+// document.write(value + "</br>");
+// var value = value + 7;
+// document.write(value + "</br>");
+// var value = --value;
+// document.write(value + "</br>");
+// document.write(value % 3);
+
+//q:4
+// var price = 600;
+// document.write("price or ticket is " + price + "</br>")
+// var quantity = 5
+// document.write("total tickets are " + quantity + "</br>")
+// document.write("total amount or tickets are " + (price*quantity))
+
+//q:no5
+// document.write("table of 4 </br>")
+// document.write("4 * 1 = 4 </br>")
+// document.write("4 * 2 = 8 </br>")
+// document.write("4 * 3 = 12 </br>")
+// document.write("4 * 4 = 16 </br>")
+// document.write("4 * 5 = 20 </br>")
+// document.write("4 * 6 = 24 </br>")
+// document.write("4 * 7 = 28 </br>")
+// document.write("4 * 8 = 32 </br>")
+// document.write("4 * 9 = 36 </br>")
+// document.write("4 * 10 = 40 </br>")
+
+//q:6
+// document.write("25'C is 77'F </br>")
+// document.write("70'F is 21.111111111'C")
+
+                 //Chapter no 6to9
+
+
+//q:1
+// var num1 = 10;
+// var num2 = --num1;
+// document.write(num2 + "</br>");
+
+// var num3 = 20;
+// var num4 = ++num3;
+// document.write(num4 + "</br>");
+
+// var num5 = 20;
+// var num5 = num5++ ;
+// document.write(num5 + "</br>");
+
+//q:2
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--
+//             //    1 - 0 + 1 + 1
+//             //          = 3 Answer
+//             document.write(result)
+
+// //q:3
+// var userName = "Enter Your name";
+// prompt(userName);
+
+//q:4
+// var userNum = +prompt("Enter Number");
+// var result = (userNum * 5);
+// document.write(result)
+
+// Q NO 6
+// var sub1 = prompt("Enter 1st Subject Name")
+// var sub2 = prompt("Enter 2nd Subject Name")
+// var sub3 = prompt("Enter 3rd Subject Name")
+// var totalMarks = 100;
+// var mark1 = +prompt("Enter obtained marks of subject1")
+// var mark2 = +prompt("Enter obtained marks of subject2")
+// var mark3 = +prompt("Enter obtained marks of subject3")
+// document.write("Sub &nbsp &nbsp &nbsp" + " Total &nbsp &nbsp" + " Obt &nbsp &nbsp" + " Percentage <br/> ")
+// document.write(sub1 + " &nbsp &nbsp &nbsp " + totalMarks+ " &nbsp &nbsp &nbsp " + mark1 + " &nbsp &nbsp &nbsp " + "%" + mark1/totalMarks*100 +"<br/>");
+// document.write(sub2 + " &nbsp &nbsp &nbsp " + totalMarks+ " &nbsp &nbsp &nbsp " + mark2 + " &nbsp &nbsp &nbsp " + "%" + mark2/totalMarks*100 +"<br/>");
+// document.write(sub3 + " &nbsp &nbsp &nbsp " + totalMarks+ " &nbsp &nbsp &nbsp " + mark3 + " &nbsp &nbsp &nbsp " + "%" + mark3/totalMarks*100);
+
+                      //Chapter no 9to11
+
+//q:1
+// var cityName = prompt("Enter City Name")
+//     if (cityName == "karachi"){
+//     alert("Welcome to the city of lights")
+//     }else{
+//     alert(" ")
+// }
+
+//q:2
+// var gender = prompt("Enter your gender")
+//     if (gender == "male"){
+//     alert("Welcome sir")
+//     }else if (gender == "female"){
+//     alert("welcome mam")
+//     }else{
+//     alert("enter gender")
+// }
+
+//q:3
+// var signalLights = prompt("Enter Signal Lights")
+//     if (signalLights == "red"){
+//     alert("Must Stop")
+//     }else if (signalLights == "yellow"){
+//     alert("ready to move")
+//     }else if (signalLights == "green"){
+//     alert("move now")
+//     }else{
+//     alert("enter right color")
+// }
+
+//q:4
+// var fuel = +prompt("enter your fuel")
+//    if (fuel < 1.25){
+//        alert("refuel your tank")
+//     }else if (fuel > 1.25){
+//     alert("suficient fuel")
+//    }else{
+//        alert("enter your fuel please")
+//    }
+
+// //q:5
+// var a = 4;
+// if (++a === 5){
+// alert("true");
+// }else{
+//   alert("false")
+// }
